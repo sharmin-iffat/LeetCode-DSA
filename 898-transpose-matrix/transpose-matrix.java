43bdda9544c4128876b8771af81719a2ee2ed4d4
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-02-14 21:29:57 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (9.34%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.java
+++ 898-transpose-matrix/transpose-matrix.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
-        int row = matrix.length, col= matrix[0].length;
-        int[][] ans = new int[col][row];
-
-        for(int i=0; i<ans.length; i++){
-            for(int j=0; j<ans[0].length; j++){
-                ans[i][j] = matrix[j][i];
+        int col = matrix[0].length;
+        int row = matrix.length;
+        int [][] arr = new int[col][row];
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                arr[j][i] = matrix[i][j];
             }
         }
-        return ans;
+        return arr;
     }
 }
