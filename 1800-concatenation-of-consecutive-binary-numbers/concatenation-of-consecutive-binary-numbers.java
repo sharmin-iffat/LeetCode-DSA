8390060901f6e95e25f1d7890849a91c6e2fb9ef
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-02-28 22:27:54 +0530

    Time: 161 ms (36.05%) | Memory: 46.7 MB (18.60%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        long result = 0;
+        int mod = 1000000007;
+        
+        for (int i = 1; i <= n; i++) {
+            int length = Integer.toBinaryString(i).length();
+            result = ((result << length) + i) % mod;
+        }
+        
+        return (int) result;
+    }
+}
