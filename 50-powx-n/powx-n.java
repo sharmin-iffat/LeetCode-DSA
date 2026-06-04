71982f22ab1876304bb72582e08f02ebabed88d0
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-06-04 15:50:34 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (90.14%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    private double power(double x, long n){
+        if(n==0) return 1;
+
+        double call = power(x, n/2);
+        
+        if(n%2 == 0 ) return call * call;
+        else return x * call * call;
+    }
+    public double myPow(double x, int n) {
+        long N = n;
+        if(n<0){
+            x = 1/x;
+            N = -N;
+        }
+        return power(x,N);
+    }
+}
+    
