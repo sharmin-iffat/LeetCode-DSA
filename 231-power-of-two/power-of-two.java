70b71263ebfcd4ee06b7b1b393eb61f775213e88
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-06-28 00:30:17 +0530

    Time: 1 ms (96.43%) | Memory: 42.7 MB (35.98%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==1) return true;
-        if(n%2 != 0 || n == 0) return false;
-        
+        if(n == 0) return false;
+        if(n < 0) return false;
 
-        return isPowerOfTwo(n/2);
-
+        return ((n & (n - 1)) == 0);
     }
 }
