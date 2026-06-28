7096b092dffd540100a63457f2e161cac7def00f
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-06-28 22:31:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (15.64%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,15 @@
 class Solution {
+    int[] dp;
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1 || n==2) return 1;
+        dp = new int[n+1];
+        return fibo(n);
+    }
+    public int fibo(int n){
+        if(n<=1) return n;
 
-        return fib(n-1) + fib(n-2);
+        if(dp[n] != 0) return dp[n];
+        int ans = fibo(n-1) + fibo(n-2);
+        dp[n] = ans;
+        return ans;
     }
 }
