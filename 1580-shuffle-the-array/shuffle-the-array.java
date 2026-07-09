dc49514eb80d2fb6af401bfbfa3c44056f709580
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-07-09 23:07:47 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (41.47%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2*n];
+        int i = 0, j=n, k = 0;
+
+        while(j < 2 * n){
+            ans[k++] = nums[i++];
+            ans[k++] = nums[j++];
+        }
+        return ans;
+    }
+}
