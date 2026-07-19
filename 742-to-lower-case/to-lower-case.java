6378380637cc8bcbcd7d674b150bce18eb29411b
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-07-19 14:52:39 +0530

    Time: 3 ms (10.53%) | Memory: 43.3 MB (9.02%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String ans = "";
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            
+            if(ch >= 'A' && ch <= 'Z'){
+                ch = (char)(ch + 32);
+            }
+            ans += ch;
+        }
+        return ans;
+        
+    }
+}
