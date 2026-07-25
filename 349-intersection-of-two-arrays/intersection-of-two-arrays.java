5dfe9043a0ecfa26d2dfd0835861bfe4f25e8f30
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-07-25 14:13:29 +0530

    Time: 6 ms (21.34%) | Memory: 45.2 MB (12.70%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,20 +1,28 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         int n = Math.min(nums1.length, nums2.length);
-        
-        int[] arr = new int[n];
-        HashSet<Integer> set = new HashSet<>();
+        int[] ans = new int[n];
 
-        for(int num:nums1){
-            set.add(num);
-        }
-        int idx =0;
-        for(int num:nums2){
-            if(set.contains(num)){
-                arr[idx++] = num;
-                set.remove(num);
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i=0, j=0, k=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i] == nums2[j]){
+                if(k == 0){
+                    ans[k++] = nums1[i];
+                }else if(k != 0 && nums1[i] != ans[k-1]){
+                    ans[k++] = nums1[i];
+                }
+                i++;
+                j++;
+            }else if(nums1[i] < nums2[j]){
+                i++;
+            }else{
+                j++;
             }
         }
-        return Arrays.copyOf(arr, idx);
+        return Arrays.copyOf(ans, k);
+        
     }
 }
