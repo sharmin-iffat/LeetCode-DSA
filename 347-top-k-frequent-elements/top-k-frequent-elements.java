9a8026bce8ecfa923e84cb03e3905e7df34376c1
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-07-26 14:54:44 +0530

    Time: 18 ms (12.83%) | Memory: 50.6 MB (5.92%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -3,19 +3,30 @@ class Solution {
         int[] ans = new int[nums.length];
 
         HashMap<Integer, Integer> mp = new HashMap<>();
+        PriorityQueue<ArrayList<Integer>> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(b.get(1), a.get(1))
+        );
 
         for(int num : nums){
             mp.put(num, mp.getOrDefault(num, 0) + 1);
         }
-        int idx = 0;
-        while(k != 0){
-            int key = mp.entrySet().stream()
-                .max(Map.Entry.comparingByValue())
-                .get()
-                .getKey();
 
+        while(mp.size() > 0){
+            ArrayList<Integer> list = new ArrayList<>();
+            int key = mp.keySet().iterator().next();
+            int value = mp.get(key);
+            list.add(key);
+            list.add(value);
+            pq.add(list);
             mp.remove(key);
-            ans[idx++] = key;
+        }
+
+        int idx = 0;
+        int i =0;
+        while(k != 0){
+            ArrayList<Integer> list = pq.remove();
+            int val = list.get(i);
+            ans[idx++] = val;
             k--;
         }
         return Arrays.copyOf(ans, idx);
