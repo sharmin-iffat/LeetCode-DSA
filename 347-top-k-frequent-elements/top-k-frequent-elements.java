2d4e1607a519d0958dbad9d08977021e46e54980
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-07-26 15:12:16 +0530

    Time: 16 ms (38.51%) | Memory: 47.9 MB (19.56%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,34 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int[] ans = new int[nums.length];
+        HashMap<Integer,Integer> mp = new HashMap<>();
 
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        PriorityQueue<ArrayList<Integer>> pq = new PriorityQueue<>(
-            (a, b) -> Integer.compare(b.get(1), a.get(1))
-        );
-
-        for(int num : nums){
-            mp.put(num, mp.getOrDefault(num, 0) + 1);
+        for(int num:nums){
+            mp.put(num, mp.getOrDefault(num, 0)+1);
         }
 
-        while(mp.size() > 0){
-            ArrayList<Integer> list = new ArrayList<>();
-            int key = mp.keySet().iterator().next();
-            int value = mp.get(key);
-            list.add(key);
-            list.add(value);
-            pq.add(list);
-            mp.remove(key);
-        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+        (a,b) -> Integer.compare(mp.get(a), mp.get(b)));
 
-        int idx = 0;
-        int i =0;
-        while(k != 0){
-            ArrayList<Integer> list = pq.remove();
-            int val = list.get(i);
-            ans[idx++] = val;
-            k--;
+        for (int key : mp.keySet()) {
+            pq.offer(key);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
-        return Arrays.copyOf(ans, idx);
+        int[] ans  = new int[k];
+
+        for(int i=0; i<k; i++){
+            ans[i] = pq.remove();
+        }
+        return ans;
+        
     }
 }
