b488ddd6e4191a70fd1ac92f80596b33d93430fc
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-08-04 16:12:29 +0530

    Time: 3 ms (81.76%) | Memory: 46.7 MB (80.06%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -1,23 +1,27 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
         List<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
 
-        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
-        int i = nums[0];
-        int n = nums.length;
-        int idx = 0;
+        for(int num:nums){
+            min = Math.min(min, num);
+            max = Math.max(max, num);
 
-        while(i< nums[n-1]){
-            if(nums[idx] != i){
-                list.add(i);
-                
-            }else{
-                idx++;
-            }
-            i++;
+            set.add(num);
         }
+        int i=0;
 
+        while(min<max){
+            if(!set.contains(min)){
+                list.add(min);
+            }
+            
+            min +=1;
+        }
         return list;
+
     }
 }
