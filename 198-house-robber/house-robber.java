fc972fc826679681a73bc4508981fbade26a46ef
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-08-04 21:14:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (75.80%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -10,6 +10,6 @@ class Solution {
         for(int i=2; i<n; i++){
             dp[i]= Math.max(dp[i-1], dp[i-2] + nums[i]);
         }
-        return Math.max(dp[n-1], dp[n-2]);
+        return dp[n-1];
     }
 }
