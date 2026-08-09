b118723c28c25e67bc4aeff7ae53952768592fbd
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-08-09 22:04:08 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (86.51%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,15 +14,17 @@
  * }
  */
 class Solution {
-    public void dfs(TreeNode root, List<Integer> list) {
-        if(root == null) return;
-        dfs(root.left, list);
-        dfs(root.right, list);
-        list.add(root.val);
-    }
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>(); 
-        dfs(root, list);
+        List<Integer> list = new ArrayList<>();
+
+        helper(root, list);
         return list;
+    }
+    public void helper(TreeNode root, List<Integer> list) {
+        if(root == null) return;
+
+        helper(root.left, list);
+        helper(root.right, list);
+        list.add(root.val);
     }
 }
